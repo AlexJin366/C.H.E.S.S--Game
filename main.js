6cfe39a7986b9446da7051a2e14ddb4819ddd4b8
AlexJin366 <1015461378@qq.com>
2020-02-23 00:19:58 -0500

    Fix validation move for Pawn

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function clearMoveMade(){
 }
 
 function movePiece(element, childId){
-    document.getElementById(piece.position).src = "";
+    document.getElementById(piece.position).removeAttribute("src");
     document.getElementById(childId).src = piece.source;
     clearMoveMade();
     piece.position = childId;
@@ -72,9 +72,9 @@ class Pawn {
     }
 
     highlightMoves(validMoves){
-        // console.log(validMoves);
-        for(let i = 0; i < validMoves.length; i++)
-            document.getElementById(validMoves[i]).parentElement.style.background = "#bfbc9f"; 
+        for(let i = 0; i < validMoves.length; i++){
+            document.getElementById(validMoves[i]).parentElement.style.background = "#bfbc9f";  
+        }
     }
 
     getMoveArray(){
@@ -96,15 +96,27 @@ class Pawn {
         }
         if(this.type == "black"){
             if(this.default){
-                this.getMoveArray().push(currentPos += 10, currentPos += 10);
+                if (document.getElementById((currentPos + 10).toString()).src == "" && document.getElementById((currentPos + 20).toString()).src == "") {
+                    this.getMoveArray().push(currentPos += 10, currentPos += 10);
+                } else if (document.getElementById((currentPos + 20).toString()).src != "" && document.getElementById((currentPos + 10).toString()).src == "") {
+                    this.getMoveArray().push(currentPos += 10);
+                }
             }else{
-                this.getMoveArray().push(currentPos += 10);
+                if(document.getElementById((currentPos+10).toString()).src == ""){
+                    this.getMoveArray().push(currentPos += 10);
+                }
             }
         }else{
             if(this.default){
-                this.getMoveArray().push(currentPos -= 10, currentPos -= 10);
+                if (document.getElementById((currentPos - 10).toString()).src == "" && document.getElementById((currentPos - 20).toString()).src == ""){
+                    this.getMoveArray().push(currentPos -= 10, currentPos -= 10);
+                } else if (document.getElementById((currentPos - 20).toString()).src != "" && document.getElementById((currentPos - 10).toString()).src == "" ){
+                    this.getMoveArray().push(currentPos -= 10);
+                }
             }else{
-                this.getMoveArray().push(currentPos -= 10);
+                if (document.getElementById((currentPos - 10).toString()).src == "") {
+                    this.getMoveArray().push(currentPos -= 10);
+                }
             }
         }
         moveOptions = this.getMoveArray();
