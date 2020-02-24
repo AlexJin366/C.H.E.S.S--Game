fe133127d81bf670b3c5708c393246cf9c85b18d
hp396 <48489886+hp396@users.noreply.github.com>
2020-02-23 19:59:57 -0500

    update run

--- run.py
+++ run.py
@@ -1,25 +1,68 @@
-from flask import Flask, render_template, send_file
-from flask_socketio import SocketIO
-import os
-import json
+#from flask import Flask 
+#from flask_socketio import SocketIO, send
+
+#app = Flask(__name__)
+#socketio = SocketIO(app)
+
+#@socketio.on('message')
+#def handleMessage(msg):
+#	print('Message: ' + msg)
+#	send(msg, broadcast=True)
+
+#if __name__ == '__main__':
+#	socketio.run(app)
+
+
+
+from flask import Flask, render_template #, send_file, request
+from flask_socketio import SocketIO,send, emit#,join_room, leave_room
 from Player import Player
+
 
 app = Flask(__name__, static_folder='public', static_url_path='')
 socketio = SocketIO(app)
 
-def player():
+def addplayer():
     global PlayerA
+    global PlayerB
     global number_users
+    global current_user
     if PlayerA == 0:
         PlayerA = Player("Player A", True)
-        number_users += 1
+        connect(PlayerA)
         return
-    global PlayerB
-    if PlayerB == 0:
-        playerB = Player("Player B",False)
-        number_users += 1
+    elif PlayerB == 0:
+        PlayerB = Player("Player B",False)
+        connect(PlayerB)
         return
+
+@socketio.on('connected')
+def connected():
+    send("DFD", broadcast=True)
+
+@socketio.on('connect')
+def connect(current_user):
+    global number_users
+    global playerslist
+    number_users += 1
+ #   print(request.namespace.socket.sessid)
+
+    playerslist.append(current_user)
+  #  socketio.emit('status', {'count': number_users}, broadcast=True)
+
+#Not disconnecting yet!
+#@socketio.on('disconnect')
+#def disconnect(current_user):
+ #   global number_users
+  #  global playerslist
+   # number_users -= 1    
+    #playerslist.remove(current_user)
+    #socketio.emit('status', {'count': number_users}, broadcast=True)
     
+#@socketio.on('message')
+#def handleMessage(msg):
+#	print('Message: ' + msg)
+#	send(msg, broadcast=True)
     
 # Handle the index (home) page
 @app.route('/')
@@ -27,19 +70,22 @@ def index():
     global number_users
     if number_users>=2:
         return render_template('cannotplay.html')
-    player()
+    addplayer()
     return render_template('index.html')
 
+#@app.route("/", methods=["GET"])
+#def get_my_ip():
+#    return request.remote_addr
 
 
 # Any additional handlers that go beyond simply loading a template
 # (e.g., a handler that needs to pass data to a template) can be added here
 
-
 if __name__ == "__main__":
+    playerslist = []
     PlayerA = 0
     PlayerB = 0
-    
+    current_user = 0
     number_users = 0
-    socketio.run(app,host='10.250.11.180', port=880, debug=True)
+    socketio.run(app,debug=True)
     #app.run(host='10.250.11.180', port=880, debug=True)
