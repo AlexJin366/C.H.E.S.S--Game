76da31f937ddce681e952023c83aa868331ee8ce
AlexJin366 <1015461378@qq.com>
2020-02-23 23:40:58 -0500

    Refactor Knight

--- Knight.js
+++ Knight.js
@@ -1,57 +1,10 @@
-class Knight {
+class Knight extends Piece{
     constructor(position, source, type) {
-        this.position = position;
-        this.source = source;
-        this.type = type;
-        this.default = true;
-        this.validMoves = new Array();
-    }
-    getPosition() {
-        //   console.log("My position is on " + this.position);
-        return this.position;
-    }
-
-    setPosition(newPos) {
-        this.position = newPos;
-    }
-
-    getSource() {
-        return this.source
-    }
-
-    highlightMoves(validMoves) {
-        // console.log(validMoves);
-        for (let i = 0; i < validMoves.length; i++)
-            document.getElementById(validMoves[i]).parentElement.style.background = "#bfbc9f";
-    }
-
-    getMoveArray() {
-        return this.validMoves;
-    }
-
-    clean() {
-        this.validMoves = new Array();
-    }
-
-    checkCapture(position){
-        switch(this.type){
-            case "black":
-                if(document.getElementById(position.toString()).src.includes("Pices/White/")){
-                    this.getMoveArray().push(position);
-                }
-                break;
-            case "white":
-                if(document.getElementById(position.toString()).src.includes("Pices/Black/")){
-                    this.getMoveArray().push(position);
-                }
-                break;
-        }
-        // console.log(position);
+        super(position,source,type);
     }
 
     getValidMoves() {
         let currentPos = Number(this.position);
-        console.log("------" + currentPos);
         if (selectedPiece != this.position) {
             oldSelectedPiece = selectedPiece;
             selectedPiece = this.position;
@@ -77,7 +30,6 @@ class Knight {
                     continue;
                 }
                 if(document.getElementById(option.toString()).src == ""){
-                    // console.log(option);
                     this.getMoveArray().push(option);
                 }else{
                     this.checkCapture(option);
