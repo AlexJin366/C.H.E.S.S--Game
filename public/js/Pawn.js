deaf97b59d996fe2e8eebe6a80e427e6085c5d4b
hp396 <48489886+hp396@users.noreply.github.com>
2020-03-05 21:59:51 -0500

    after merge

---
+++ public/js/Pawn.js
@@ -0,0 +1,77 @@
+class Pawn extends Piece{
+    constructor(position, source, type) {
+        super(position,source,type);
+    }
+
+    checkCapture(){
+        let currentPos = Number(this.position);
+        if (selectedPiece != this.position) {
+            oldSelectedPiece = selectedPiece;
+            selectedPiece = this.position;
+            clearValidMoves();
+            this.clean();
+        }
+        if(this.type == "black"){
+            if(((currentPos + 9) < (Math.floor((currentPos+10) / 10) * 10 + 9)) && (currentPos + 9) > (Math.floor((currentPos+10) / 10) * 10)){
+                if(document.getElementById((currentPos + 9).toString()).src != "" && document.getElementById((currentPos + 9).toString()).src.includes("/Pieces/White/")){
+                    this.getMoveArray().push(currentPos + 9);
+                }
+            }
+            if(((currentPos + 11) < (Math.floor((currentPos+10) / 10) * 10 + 9)) && (currentPos + 11) > (Math.floor((currentPos+10) / 10) * 10)){
+                if(document.getElementById(currentPos + 11).toString().src != "" && document.getElementById((currentPos + 11).toString()).src.includes("/Pieces/White/")){
+                    this.getMoveArray().push(currentPos + 11);
+                }
+            }
+        }else{
+            if(((currentPos - 9) < (Math.floor((currentPos-10) / 10) * 10 + 9)) && (currentPos - 9) > (Math.floor((currentPos-10) / 10) * 10)){
+                if(document.getElementById((currentPos - 9).toString()).src != "" && document.getElementById((currentPos - 9).toString()).src.includes("/Pieces/Black/")){
+                    this.getMoveArray().push(currentPos - 9);
+                }
+            }
+            if(((currentPos - 11) < (Math.floor((currentPos-10) / 10) * 10 + 9)) && (currentPos - 11) > (Math.floor((currentPos-10) / 10) * 10)){
+                if(document.getElementById(currentPos - 11).toString().src != "" && document.getElementById((currentPos - 11).toString()).src.includes("/Pieces/Black/")){
+                    this.getMoveArray().push(currentPos - 11);
+                }
+            }
+        }
+    }
+
+    getValidMoves() {
+        let currentPos = Number(this.position);
+        if (selectedPiece != this.position) {
+            oldSelectedPiece = selectedPiece;
+            selectedPiece = this.position;
+            clearValidMoves();
+            this.clean();
+        }
+        if (this.type == "black") {
+            if (this.default) {
+
+                if (document.getElementById((currentPos + 10).toString()).src == "" && document.getElementById((currentPos + 20).toString()).src == "") {
+                    this.getMoveArray().push(currentPos += 10, currentPos += 10);
+                } else if (document.getElementById((currentPos + 20).toString()).src != "" && document.getElementById((currentPos + 10).toString()).src == "") {
+                    this.getMoveArray().push(currentPos += 10);
+                }
+            } else {
+                if (document.getElementById((currentPos + 10).toString()).src == "") {
+                    this.getMoveArray().push(currentPos += 10);
+                }
+            }
+        } else {
+            if (this.default) {
+                if (document.getElementById((currentPos - 10).toString()).src == "" && document.getElementById((currentPos - 20).toString()).src == "") {
+                    this.getMoveArray().push(currentPos -= 10, currentPos -= 10);
+                } else if (document.getElementById((currentPos - 20).toString()).src != "" && document.getElementById((currentPos - 10).toString()).src == "") {
+                    this.getMoveArray().push(currentPos -= 10);
+                }
+            } else {
+                if (document.getElementById((currentPos - 10).toString()).src == "") {
+                    this.getMoveArray().push(currentPos -= 10);
+                }
+            }
+        }
+        this.checkCapture();
+        moveOptions = this.getMoveArray();
+        this.highlightMoves(this.getMoveArray());
+    }
+}
---
+++ public/js/Rook.js
@@ -0,0 +1,70 @@
+class Rook extends Piece{
+    constructor(position, source, type) {
+        super(position,source,type);
+    }
+
+    getValidMoves() {
+        
+        let currentPos = Number(this.position);
+        if (selectedPiece != this.position) {
+            oldSelectedPiece = selectedPiece;
+            selectedPiece = this.position;
+            clearValidMoves();
+            this.clean();
+        }
+
+        let movesUp = 90 - currentPos;
+
+        //moves down
+        for (let i = 10; i < movesUp; i += 10) {
+            let option = currentPos + i;
+            if (document.getElementById(option.toString()).src != ""){
+                this.checkCapture(option);
+                break;
+            }
+            if (option > 10)
+                this.getMoveArray().push(option);
+        }
+
+        //moves up
+        for (let i = 10; i < 90; i += 10) {
+            let option = currentPos - i;
+            if (option > 10 && document.getElementById(option.toString()).src == "")
+                this.getMoveArray().push(option);
+            if (option > 10 && document.getElementById(option.toString()).src != ""){
+                this.checkCapture(option);
+                break;
+            }
+        }
+
+        //moves left
+        for (let i = 1; i < 9; i += 1) {
+            let option = currentPos - i;
+            let mathRequirement1 = (Math.floor(currentPos / 10) * 10 + 9);
+            let mathRequirement2 = (Math.floor(currentPos / 10) * 10);
+            if (option > mathRequirement2 && option < mathRequirement1 && document.getElementById(option.toString()).src == "")
+                this.getMoveArray().push(option);
+            if (option > mathRequirement2 && option < mathRequirement1 && document.getElementById(option.toString()).src != ""){
+                this.checkCapture(option);
+                break;
+            }
+        }
+
+        //moves right
+        for (let i = 1; i < 9; i += 1) {
+            let option = currentPos + i;
+            let mathRequirement1 = (Math.floor(currentPos / 10) * 10 + 9);
+            let mathRequirement2 = (Math.floor(currentPos / 10) * 10);
+            if (option > mathRequirement2 && option < mathRequirement1 && document.getElementById(option.toString()).src == "")
+                this.getMoveArray().push(option);
+            if (option > mathRequirement2 && option < mathRequirement1 && document.getElementById(option.toString()).src != ""){
+                this.checkCapture(option);
+                break
+            }
+        }
+
+        moveOptions = this.getMoveArray();
+        this.highlightMoves(this.getMoveArray());
+        
+    }
+}
