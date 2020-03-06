122c13d93ef441574c47c54e2c7af604c6cebffb
Afshara Tasnim <at992@drexel.edu>
2020-03-06 18:49:48 -0500

    pawn test

--- Selenium/movementTest.py
+++ Selenium/movementTest.py
@@ -10,26 +10,41 @@ from selenium.webdriver.common.action_chains import ActionChains
 # import Bishop.js as Bishop
 # #Board,King,Knight,main,Pawn,Piece,Queen,Rook
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\Harsh\Documents\GitHub\C.H.E.S.S--Game\Selenium\drivers\chromedriver.exe')
+driver = webdriver.Chrome(executable_path=r'C:/Users/Owner/Documents/GitHub/C.H.E.S.S--Game/Selenium/drivers/chromedriver.exe')
 driver.set_page_load_timeout(10)
 actions = ActionChains(driver)
 
 driver.get("http://127.0.0.1:5000/")
 
+def joingame():
+    driver.find_element_by_class_name("btn").click()
+    return
+
 
 def pawnMovement():
-    driver.find_element_by_class_name("btn").click()
-    driver.find_element_by_id("21").click()
+
+    #driver.find_element_by_id("21").click()
     menu = driver.find_element_by_id("21")
+    menu.click()
     hidden_submenu = driver.find_element_by_id("31")
     actions.move_to_element(menu)
     actions.click(hidden_submenu)
     actions.perform()
-    #actions(driver).move_to_element(menu).click(hidden_submenu).perform()
-
-    #driver.find_element_by_id("31").click()
-    driver.find_element_by_id("22").click()
+    #driver.find_element_by_id("22").click()
     return
 
-pawnMovement()
 
+
+def rookMovement():
+    menu = driver.find_element_by_id("12")
+    menu.click()
+    hidden_submenu = driver.find_element_by_id("33")
+    actions.move_to_element(menu)
+    actions.click(hidden_submenu)
+    actions.perform()
+    #driver.find_element_by_id("22").click()
+    return
+
+joingame()
+pawnMovement()
+rookMovement()
