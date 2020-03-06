cd0d6c99af56810f3e19072c8884e87ec5dfff02
Afshara Tasnim <at992@drexel.edu>
2020-03-06 17:04:31 -0500

    update movementTest.py

--- Selenium/movementTest.py
+++ Selenium/movementTest.py
@@ -1,24 +1,24 @@
 from  selenium import webdriver
 import Convertor
 import sys
-sys.path.append(r"C:\Users\Owner\Documents\GitHub\C.H.E.S.S--Game\js")
-print("\nPaths\n")
-print(sys.path)
-print("\n")
-import Bishop.js as Bishop
-#Board,King,Knight,main,Pawn,Piece,Queen,Rook
-
-
+# sys.path.append(r"C:\Users\Owner\Documents\GitHub\C.H.E.S.S--Game\js")
+# print("\nPaths\n")
+# print(sys.path)
+# print("\n"):
+# import Bishop.js as Bishop
+# #Board,King,Knight,main,Pawn,Piece,Queen,Rook
 
 driver = webdriver.Chrome(executable_path=r'C:\Users\Owner\Documents\GitHub\C.H.E.S.S--Game\Selenium\drivers\chromedriver.exe')
 driver.set_page_load_timeout(10)
 
-driver.get("file:///C:/Users/Owner/Documents/GitHub/C.H.E.S.S--Game/html/index.html")
+driver.get("http://127.0.0.1:5000/")
+
 
 def pawnMovement():
+    driver.find_element_by_class_name("btn").click()
     driver.find_element_by_id("21").click()
-    driver.find_element_by_id("31").click()
-    return;
+    #driver.find_element_by_id("31").click()
+    return
 
-
+pawnMovement()
 
